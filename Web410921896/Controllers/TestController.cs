ae3c889ce7c618fde8b3017c048987b81ca19a0d
楊舒語 <duduyang00276@gmail.com>
2023-05-03 19:41:52 +0800

    first commit

---
+++ Web410921896/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Web410921896.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web410921896/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web410921896.MvcApplication" Language="C#" %>
