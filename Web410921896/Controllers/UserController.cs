d282f9ae5481ac167b49ac42bc2342d5e6822abb
楊舒語 <duduyang00276@gmail.com>
2023-05-17 20:45:15 +0800

    HW2

--- Web410921896/Controllers/UserController.cs
+++ Web410921896/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,22 @@ namespace Web410921896.Controllers
         [HttpPost]
         public ActionResult SignUp(string name,string account, string password)
         {
+            if (name.IsNullOrWhiteSpace())
+            {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            if(account.IsNullOrWhiteSpace())
+            {
+                ViewBag.AccountMessage = "請輸入帳號";
+            }
+            if(password.IsNullOrWhiteSpace())
+            {
+                ViewBag.PasswordMessage = "請輸入密碼";
+            }
+            if(!name.IsNullOrWhiteSpace() && !account.IsNullOrWhiteSpace() && !password.IsNullOrWhiteSpace())
+            {
+                ViewBag.Message = "註冊成功";
+            }
             return View();
         }
     }
