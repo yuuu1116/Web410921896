dcd6f579c067ece9211da0254438f1c69b44f15b
楊舒語 <duduyang00276@gmail.com>
2023-05-17 20:30:15 +0800

    註冊

---
+++ Web410921896/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410921896.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(string name,string account, string password)
+        {
+            return View();
+        }
+    }
+}
