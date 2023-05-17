8ec6b02dd09160cd18575900c735e5f77e2a4df5
楊舒語 <duduyang00276@gmail.com>
2023-05-17 19:57:32 +0800

    HW1

--- Web410921896/Controllers/WaterController.cs
+++ Web410921896/Controllers/WaterController.cs
@@ -9,8 +9,15 @@ namespace Web410921896.Controllers
     public class WaterController : Controller
     {
         // GET: Water
-        public ActionResult Index()
+        public ActionResult Index(string name,float? weight)
         {
+            var result = "";
+            if (weight.HasValue)
+            {
+                result = (weight * 30) + "cc";
+            }
+            ViewBag.name = name;
+            ViewBag.result = result;
             return View();
         }
     }
